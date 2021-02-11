a9f9a480a8035b3936eaf33f6ac732403c036f61
Dávid Slačka <Hamsik123159@gmail.com>
2021-02-11 13:24:03 +0100

    v0.01
    - initialisation

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1920, initial-scale=1.0">
+    <title>DarkStarGalaxy</title>
+    <link rel="stylesheet" href="Style/main.css">
+</head>
+<body>
+    <header>
+        <h2>hello world</h2>
+    </header>
+</body>
+</html>
